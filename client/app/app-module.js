308ec9dce0c9e751942586753a58049b35722b70
Robinyo <rob.ferguson@robferguson.org>
2016-01-03 14:19:01 +1100

    First commit

---
+++ client/app/app-module.js
@@ -0,0 +1,9 @@
+(function() {
+
+  'use strict';
+
+  // 'ui.router'  'ngResource'  'ui.bootstrap'
+
+  angular.module('my-2d-diagram-editor', []);
+
+})();
---
+++ client/app/hello-world-controller.js
@@ -0,0 +1,10 @@
+(function() {
+
+  'use strict';
+
+  angular.module('my-2d-diagram-editor', []).
+    controller('HelloWorldController',function ($scope) {
+      $scope.name = { first: "Rob", last: "Ferguson" };
+    });
+
+})();
