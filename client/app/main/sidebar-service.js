b5f4ec2e5690288fd932b76b0c643c094fa09282
Robinyo <rob.ferguson@robferguson.org>
2016-01-10 16:36:33 +1100

    Updated the default colour palette

--- client/app/main/sidebar-service.js
+++ client/app/main/sidebar-service.js
@@ -22,7 +22,9 @@
             "value"           : "Square",
             "src"             : "content/images/icons/32x32/light-gray.png",
             "backgroundColor" : "LIGHTGRAY"
-          },
+          }
+
+          /* ,
           {
             "id"              : RECTANGLE_ID,
             "name"            : "RECTANGLE",
@@ -30,6 +32,8 @@
             "src"             : "content/images/icons/32x32/light-gray.png",
             "backgroundColor" : "LIGHTGRAY"
           }
+          */
+
         ];
 
         var containers = [
@@ -37,36 +41,41 @@
             "id"              : CONTROLLED_ZONE_ID,
             "name"            : "CONTROLLED_ZONE",
             "value"           : "Controlled Zone",
-            "src"             : "content/images/icons/32x32/yellow.png",
-            "backgroundColor" : "YELLOW"
+            // "src"             : "content/images/icons/32x32/yellow.png",
+            "src"             : "http://placehold.it/32x32/E3F09B",
+            "backgroundColor" : "#E3F09B"  // YELLOW
           },
           {
             "id"              : RESTRICTED_ZONE_ID,
             "name"            : "RESTRICTED_ZONE",
             "value"           : "Restricted Zone",
-            "src"             : "content/images/icons/32x32/light-sea-green.png",
-            "backgroundColor" : "LIGHTSEAGREEN"
+            // "src"             : "content/images/icons/32x32/light-sea-green.png",
+            "src"             : "http://placehold.it/32x32/F7D08A",
+            "backgroundColor" : "#F7D08A"  // LIGHTGREEN
           },
           {
             "id"              : SECURED_ZONE_ID,
             "name"            : "SECURED_ZONE",
             "value"           : "Secured Zone",
-            "src"             : "content/images/icons/32x32/dark-sea-green.png",
-            "backgroundColor" : "DARKSEAGREEN"
+            // "src"             : "content/images/icons/32x32/dark-sea-green.png",
+            "src"             : "http://placehold.it/32x32/87B6A7",
+            "backgroundColor" : "#87B6A7"  // GREEN
           },
           {
             "id"              : PARTNER_ZONE_ID,
             "name"            : "PARTNER_ZONE",
             "value"           : "Partner Zone",
-            "src"             : "content/images/icons/32x32/light-salmon.png",
-            "backgroundColor" : "LIGHTSALMON"
+            // "src"             : "content/images/icons/32x32/light-salmon.png",
+            "src"             : "http://placehold.it/32x32/F79F79",
+            "backgroundColor" : "#F79F79"  // LIGHTSALMON
           },
           {
             "id"              : MANAGEMENT_ZONE_ID,
             "name"            : "MANAGEMENT_ZONE",
             "value"           : "Management Zone",
-            "src"             : "content/images/icons/32x32/dodger-blue.png",
-            "backgroundColor" : "DODGERBLUE"
+            // "src"             : "content/images/icons/32x32/dodger-blue.png",
+            "src"             : "http://placehold.it/32x32/5B5941",
+            "backgroundColor" : "#5B5941"  // DODGERBLUE
           }
         ];
 
@@ -90,7 +99,8 @@
 
 /*
 
- // http://htmlcolorcodes.com/color-names/
- "src" : "http://placehold.it/32x32/FFFF00",  // YELLOW
+  // http://www.awwwards.com/trendy-web-color-palettes-and-material-design-color-schemes-tools.html
+  // http://htmlcolorcodes.com/color-names/
+  "src" : "http://placehold.it/32x32/FFFF00",  // YELLOW
 
 */
